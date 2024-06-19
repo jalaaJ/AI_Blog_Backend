5614e11fb408923f7258df8cd47f1989ed0f0d03
jalaaJ <abusalehjalaa1@gmail.com>
2024-06-19 23:41:10 +0300

    Created the url path for the generate blog view

--- blog_generator/urls.py
+++ blog_generator/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login', views.user_login, name='login'),
     path('signup', views.user_signup, name='signup'),
-    path('logout', views.user_logout, name='logout')
+    path('logout', views.user_logout, name='logout'),
+    path('generate-blog', views.generate_blog, name='generate-blog')
 ]
