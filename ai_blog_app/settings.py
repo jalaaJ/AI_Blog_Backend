253c66314346b162d30229f97d2d8940870f196e
jalaaJ <abusalehjalaa1@gmail.com>
2024-06-19 15:31:21 +0300

    Updated the credentials for the postgres database

--- ai_blog_app/settings.py
+++ ai_blog_app/settings.py
@@ -74,13 +74,23 @@ WSGI_APPLICATION = "ai_blog_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "qoveryadmin",
+        "PASSWORD": "IvN4U-r7LhzCd8r-rzqR1fcAXkMmdkYP",
+        "HOST": "ze4574c6e-postgresql.czy6geoqgvce.eu-west-2.rds.amazonaws.com",
+        "PORT": "5432"
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
